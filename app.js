257df5c78134b0e16f66789b4cfa8b81ffeb2670
0blu3 <braelynn.luedtke@gmail.com>
2016-11-07 08:20:29 -0800

    question game is working better

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var userName = prompt("What's your name?");
-console.log("Hi, " + userName + "! Welcome to my about me page.");
+'use strict';
 
-var pet = prompt("What kind of pet do you think I have?");
+var userName = prompt('What's your name?');
+alert ('Hi, ' + userName + '! Welcome to my about me page.');
 
-if pet === "dog" {
-  console.log ("You are correct! I have a French bulldog.")
-} else { console.log("That is not correct. I have a dog.")
+var pet = prompt('What kind of pet do you think I have?');
+
+if (pet === 'dog') {
+  alert ('You are correct! I have a French bulldog.')
+} else { alert ('That is not correct. I have a dog.')
 }
 
-var diet = prompt("True or false: I am a vegan.");
-if diet === true {
-  console.log("Correct!")
-} else { console.log("Incorrect! I am a vegan.")}
-}
-
-var homeState = prompt("True or false: I am a from Montana.");
-if homeState === "true" {
-  console.log("Correct!")
-} else { console.log("Incorrect! I am from Montana.")
-}
-
-var computer = prompt("Do you think I'm a Windows or Mac person?");
-if computer == "Windows" {
-  console.log("Correct! I am a Windows girl.")
-} else if computer == "Mac" {
-  console.log("False! I prefer Windows.")
+var diet = prompt('True or false: I am a vegan.');
+if (diet === true) {
+  alert('Correct!')
 } else {
-  console.log("I didn't quite understand that answer.")
+  alert('Incorrect! I am a vegan.')}
 }
 
-var phone = prompt("Do you think I have an Android phone or an iPhone?");
-if phone == "iPhone" {
-  console.log("False, I prefer Android phones.")
-} else if phone == "Android" {
-  console.log("Correct!")
+var homeState = prompt('True or false: I am a from Montana.');
+if homeState === 'true' {
+  alert('Correct!')
+} else { alert('Incorrect! I am from Montana.')
+}
+
+var computer = prompt('Do you think I'm a Windows or Mac person?');
+if computer == 'Windows' {
+  alert('Correct! I am a Windows girl.')
+} else if (computer == 'Mac') {
+  alert('False! I prefer Windows.')
+} else {
+  alert('I didn't quite understand that answer.')
+};
+
+var phone = prompt('Do you think I have an Android phone or an iPhone?');
+if phone == 'iPhone' {
+  alert('False, I prefer Android phones.')
+} else if phone == 'Android' {
+  alert ('Correct!')
 }
