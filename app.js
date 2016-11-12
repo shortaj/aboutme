934faca46de02f0c937364b137a20cfc568fdf21
Alex <ajshort2010@hotmail.com>
2016-11-11 16:18:51 -0800

    functions for aboutme PAIR PRogrrrrrrmer

--- app.js
+++ app.js
@@ -4,96 +4,113 @@ var userName = prompt('What\'s your name?');
 alert ('Hi, ' + userName + '! Welcome to my about me page. Let\'s play a guessing game.');
 
 function answers(correctAnswers) {
-
   correctAnswers = 0;
 
   var questionsArray = ['Do you think I have a dog?', 'Am I a vegan?', 'Ture or false: I am from Montana.', 'Do you think I\'m an apple person?', 'Do you think I have an Android phone?'];
-  var answersArray = ['Correct! I have a dog named Vincent.', 'You are right!', 'You are correct!', 'True! I am a Windows person.', 'Correct!'];
+  var correctAnswersArray = ['Correct! I have a dog named Vincent.', 'You are right!', 'You are correct!', 'True! I am a Windows person.', 'Correct!'];
   var invalidAnswer = 'I didn\'t understand your answer.';
   var incorrectAnswersArray = ['False, I have a dog named Vincent.', 'False!', 'Incorrect!', 'False! I prefer Windows.', 'False! I like Android phones.'];
+  var answersArray = [['YES', 'Y'], ['YES', 'Y'], ['TRUE', 'T'], ['NO', 'N'], ['NO', 'N']];
 
-  var isAnswerCorrect = [];
-
-  var correctAnswerFunction function() {
-
+  // var isAnswerCorrect = [];
+  function runQuestions() {
+    var userAnswer = '';
+    for(var i = 0; i < questionsArray.length; i++) {
+      userAnswer = prompt(questionsArray[i]);
+      correctAnswerFunction(userAnswer, i);
+    }
+  }
+  //needs to run 5 times
+  var correctAnswerFunction = function(userAnswer, i) {
+    if(userAnswer.toUpperCase() === (answersArray[i][0] || answersArray[i][1])) {
+      console.log(correctAnswers);
+      alert(correctAnswersArray[i]);
+      correctAnswers++;
+    } else if (userAnswer.toUpperCase() !== answersArray[i][0] || answersArray[i][1]) {
+      alert(incorrectAnswersArray[i]);
+    } else {
+      alert(invalidAnswer);
+    }
   };
+  runQuestions();
 
-  var incorrectAnswerFunction function () {
+  //
+  // var incorrectAnswerFunction function () {
+  //
+  // };
 
-  };
-
-
-  var pet = prompt('Do you think I have a dog?').toUpperCase();
-  if (pet === 'YES') {
-    alert('Correct! I have a dog named Vincent.');
-    correctAnswers++;
-    alert('Your score is now ' + correctAnswers + ' out of 7.');
-    console.log(correctAnswers);
-  } else if (pet === 'NO') {
-    alert('False! I have a dog named Vincent.');
-  } else {
-    alert('I didn\'t understand your answer.');
-    };
-
-    console.log('Do you think I have a dog?');
-    console.log(pet);
-
-    var diet = prompt('Am I a vegan?').toUpperCase();
-    if (diet === 'YES') {
-      alert('You are right!');
-      correctAnswers++;
-      alert('Your score is now ' + correctAnswers + ' out of 7.');
-    } else if (diet === 'NO') {
-      alert('False!');
-    } else {
-      alert('Your answer didn\'t make sense to me.')
-    };
-
-    console.log('Am I a vegan?')
-    console.log(diet);
-
-    var homeState = prompt('True or false: I am from Montana.').toUpperCase();
-    if (homeState === 'TRUE') {
-      alert('You are correct!');
-      correctAnswers++;
-      alert('Your score is now ' + correctAnswers + ' out of 7.');
-    } else if (homeState === 'FALSE') {
-      alert('Incorrect!');
-    } else {
-      alert('Your answer doesn\'t make sense.');
-    }
-
-    console.log('True or false: I am from Montana.');
-    console.log(homeState);
-
-    var computer = prompt('Do you think I\'m an Apple person?').toUpperCase();
-    if (computer === 'NO') {
-      alert('True! I prefer Windows.');
-      correctAnswers++;
-      alert('Your score is now ' + correctAnswers + ' out of 7.');
-    } else if (computer === 'NO') {
-      alert('False! I am a Windows girl.');
-    } else {
-      alert('I didn\'t quite understand that answer.');
-    }
-
-    console.log('Do you think I\'m an Apple person?');
-    console.log(computer);
-
-    var phone = prompt('Do you think I have an Android phone?').toUpperCase();
-    if (phone === 'NO') {
-      alert('False, I prefer Android phones.');
-    } else if (phone === 'YES') {
-      alert('Correct!');
-      correctAnswers++;
-      alert('Your score is now ' + correctAnswers + ' out of 7.');
-    } else {
-      alert('That answer didn\'t make sense.')
-    }
-
-    console.log('Do you think I have an Android phone?');
-    console.log(phone);
-
+  //
+  // var pet = prompt('Do you think I have a dog?').toUpperCase();
+  // if (pet === 'YES') {
+  //   alert('Correct! I have a dog named Vincent.');
+  //   correctAnswers++;
+  //   alert('Your score is now ' + correctAnswers + ' out of 7.');
+  //   console.log(correctAnswers);
+  // } else if (pet === 'NO') {
+  //   alert('False! I have a dog named Vincent.');
+  // } else {
+  //   alert('I didn\'t understand your answer.');
+  //   };
+  //
+  //   console.log('Do you think I have a dog?');
+  //   console.log(pet);
+  //
+  //   var diet = prompt('Am I a vegan?').toUpperCase();
+  //   if (diet === 'YES') {
+  //     alert('You are right!');
+  //     correctAnswers++;
+  //     alert('Your score is now ' + correctAnswers + ' out of 7.');
+  //   } else if (diet === 'NO') {
+  //     alert('False!');
+  //   } else {
+  //     alert('Your answer didn\'t make sense to me.')
+  //   };
+  //
+  //   console.log('Am I a vegan?')
+  //   console.log(diet);
+  //
+  //   var homeState = prompt('True or false: I am from Montana.').toUpperCase();
+  //   if (homeState === 'TRUE') {
+  //     alert('You are correct!');
+  //     correctAnswers++;
+  //     alert('Your score is now ' + correctAnswers + ' out of 7.');
+  //   } else if (homeState === 'FALSE') {
+  //     alert('Incorrect!');
+  //   } else {
+  //     alert('Your answer doesn\'t make sense.');
+  //   }
+  //
+  //   console.log('True or false: I am from Montana.');
+  //   console.log(homeState);
+  //
+  //   var computer = prompt('Do you think I\'m an Apple person?').toUpperCase();
+  //   if (computer === 'NO') {
+  //     alert('True! I prefer Windows.');
+  //     correctAnswers++;
+  //     alert('Your score is now ' + correctAnswers + ' out of 7.');
+  //   } else if (computer === 'NO') {
+  //     alert('False! I am a Windows girl.');
+  //   } else {
+  //     alert('I didn\'t quite understand that answer.');
+  //   }
+  //
+  //   console.log('Do you think I\'m an Apple person?');
+  //   console.log(computer);
+  //
+  //   var phone = prompt('Do you think I have an Android phone?').toUpperCase();
+  //   if (phone === 'NO') {
+  //     alert('False, I prefer Android phones.');
+  //   } else if (phone === 'YES') {
+  //     alert('Correct!');
+  //     correctAnswers++;
+  //     alert('Your score is now ' + correctAnswers + ' out of 7.');
+  //   } else {
+  //     alert('That answer didn\'t make sense.')
+  //   }
+  //
+  //   console.log('Do you think I have an Android phone?');
+  //   console.log(phone);
+  //
 
 
 
